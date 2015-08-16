16e35be542762451e3ec36f542db3cdb49a116f0
michel <michel@miscpc01>
2015-08-16 20:59:38 +0200

    Minor details

--- touse.moplaf.solver.lp/com.misc.touse.moplaf.solver.lp.emf/src/com/misc/touse/moplaf/solver/tousesolverlp/impl/ToUseGeneratorImpl.java
+++ touse.moplaf.solver.lp/com.misc.touse.moplaf.solver.lp.emf/src/com/misc/touse/moplaf/solver/tousesolverlp/impl/ToUseGeneratorImpl.java
@@ -12,6 +12,7 @@ import com.misc.touse.moplaf.solver.tousesolverlp.ToUseTuple;
 
 import java.util.Collection;
 
+import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.notify.NotificationChain;
 
 import org.eclipse.emf.common.util.EList;
@@ -20,6 +21,7 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.util.InternalEList;
 
 /**
@@ -186,5 +188,17 @@ public class ToUseGeneratorImpl extends GeneratorImpl implements ToUseGenerator 
 		}
 		return super.eIsSet(featureID);
 	}
-
+	
+	/**
+	 */
+	@Override
+	public void generate() {
+		CommonPlugin.INSTANCE.log("ToUseGenerator.generate: called");
+		this.generateTuples();
+		CommonPlugin.INSTANCE.log("TouseGenerator.generate: tuples generated");
+		this.generateVars();
+		CommonPlugin.INSTANCE.log("ToUseGenerator.generate: vars generated");
+		this.generateCons();
+		CommonPlugin.INSTANCE.log("ToUseGenerator.generate: cons generated");
+	}
 } //ToUseGeneratorImpl
