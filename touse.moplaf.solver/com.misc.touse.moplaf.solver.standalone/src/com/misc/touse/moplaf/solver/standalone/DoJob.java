9b5779dfea82b3eb2647a92303c2b833a616274f
MichelSc <michelschaffers@gmail.com>
2016-07-09 17:22:01 +0200

    Create resource and resourceSet

--- touse.moplaf.solver/com.misc.touse.moplaf.solver.standalone/src/com/misc/touse/moplaf/solver/standalone/DoJob.java
+++ touse.moplaf.solver/com.misc.touse.moplaf.solver.standalone/src/com/misc/touse/moplaf/solver/standalone/DoJob.java
@@ -2,11 +2,12 @@ package com.misc.touse.moplaf.solver.standalone;
 
 import java.util.Arrays;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 
-import com.misc.common.moplaf.common.Job;
 import com.misc.touse.moplaf.solver.tousesolver.Domain;
 import com.misc.touse.moplaf.solver.tousesolver.SolveKnapsack;
 import com.misc.touse.moplaf.solver.tousesolver.TousesolverFactory;
@@ -16,19 +17,30 @@ public class DoJob {
 	public static void main (String[] args){
 	    System.out.println("ToUseSolver standalone started");
 	    //Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("library", new XMIResourceFactoryImpl());
+
+	    // create resourceFactory
+	    XMLResourceFactoryImpl rf = new XMLResourceFactoryImpl(); 
 	    
+	    // create the resourceSet
 	    ResourceSet rs = new ResourceSetImpl();
-	    rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new XMLResourceFactoryImpl());
+	    rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", rf);
 	    
-	    TousesolverFactory.eINSTANCE.eClass();
+	    // create the resource
+	    URI uri = URI.createURI("http://www.misc.com/touse/moplaf/solver/standalone/job.xml");
+	    Resource resource = rs.createResource(uri);
 	    
+	    // create the domain
+	    //TousesolverFactory.eINSTANCE.eClass();
 	    Domain newDomain = TousesolverFactory.eINSTANCE.createDomain();
+	    resource.getContents().add(newDomain);
 	    
+	    // create the job
 	    SolveKnapsack newJob = TousesolverFactory.eINSTANCE.createSolveKnapsack();
 	    newJob.getArgs().addAll(Arrays.asList(args));
 	    newJob.setName("Jog SolveKnapack");
 	    newDomain.getJobs().add(newJob);
 	    
+	    // start the job
 	    newJob.start();
 	    
 	    System.out.println("ToUseSolver standalone finished");
