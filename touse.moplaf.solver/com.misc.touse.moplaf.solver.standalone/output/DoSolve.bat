c02d1602ed70badf78c23545218c7687485097c6
misc <MiSc@ORD01777.ordina.belgium>
2016-07-13 12:09:19 +0200

    Start executable script

--- touse.moplaf.solver/com.misc.touse.moplaf.solver.standalone/output/DoSolve.bat
+++ touse.moplaf.solver/com.misc.touse.moplaf.solver.standalone/output/DoSolve.bat
@@ -1,4 +1,3 @@
 echo "DoSolve started"
-export LD_LIBRARY_PATH=/home/michel/git/common.moplaf/common.moplaf.solver.cplex/com.misc.common.moplaf.solver.cplex.emf/lib/x86_64-linux/
 java -jar SolveKnapsack.jar 12.36
 echo "DoSolve finished"
