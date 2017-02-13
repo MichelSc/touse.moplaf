a3cbc2a5f092d64edcf57e041090fb040fab68f9
MiSc <MiSc@ORD02652.ordina.belgium>
2017-02-13 14:53:25 +0100

    first complete version after bindings rework

--- touse.moplaf.propagator/com.misc.touse.moplaf.propagator2.design/src/com/misc/touse/moplaf/propagator2/design/ToUsePropagatorResourceSetFactory.java
+++ touse.moplaf.propagator/com.misc.touse.moplaf.propagator2.design/src/com/misc/touse/moplaf/propagator2/design/ToUsePropagatorResourceSetFactory.java
@@ -9,7 +9,6 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.sirius.common.tools.api.resource.ResourceSetFactory;
 
-import com.misc.common.moplaf.propagator2.util.PropagatorFunctionManagerAdapter;
 import com.misc.touse.moplaf.tousepropagator2calc.util.ToUsePropagatorPropagatorFunctionManager;
 
 public class ToUsePropagatorResourceSetFactory extends ResourceSetFactory {
