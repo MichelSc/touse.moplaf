716422d7bb8adcdcabf19739a35c15a1daf0e726
michel <michel@misclap01>
2017-08-14 16:20:15 +0200

    work in progress

--- touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf/src/com/misc/touse/moplaf/tousescheduler/impl/CalcToUseSolutionTaskExpressionDistanceImpl.java
+++ touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf/src/com/misc/touse/moplaf/tousescheduler/impl/CalcToUseSolutionTaskExpressionDistanceImpl.java
@@ -50,6 +50,7 @@ public class CalcToUseSolutionTaskExpressionDistanceImpl extends SetTaskExpressi
 		
 	private static Bindings thisTaskBindings = Bindings.constructBindings()
 			.addInboundBinding(SchedulerPackage.Literals.SOLUTION_TASK__CANDIDATE_PREVIOUS_TASK, taskBeforeBindings)	
+			.addInboundBinding(SchedulerPackage.Literals.SOLUTION_TASK__RESOURCE_AS_CANDIDATE_FIRST_TASK)	
 			;
 
 	private static Bindings thisTaskExpressionDistanceBindings = Bindings.constructBindings()
