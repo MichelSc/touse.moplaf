1100ab8a116ae0ee27f9272b467366241312c93d
MiSc <MiSc@ORD02652.ordina.belgium>
2017-10-04 17:56:54 +0200

    Work in progress scheduler

--- touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf.edit/src/com/misc/touse/moplaf/tousescheduler/provider/CalcResourceDistanceItemProvider.java
+++ touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf.edit/src/com/misc/touse/moplaf/tousescheduler/provider/CalcResourceDistanceItemProvider.java
@@ -70,17 +70,6 @@ public class CalcResourceDistanceItemProvider extends CalcResourceItemProvider {
 	}
 
 	/**
-	 * This returns CalcResourceDistance.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/CalcResourceDistance"));
-	}
-
-	/**
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
