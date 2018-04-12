e73b02f19fd74c751a80213ed3caf5d24d8c63b2
MiSc <MiSc@ORD02652.ordina.belgium>
2018-04-12 15:51:56 +0200

    Implement menu preferences

--- touse.moplaf.job/com.misc.touse.moplaf.job.emf.editor/src/com/misc/touse/moplaf/tousejob/presentation/ToUseJobActionBarContributor.java
+++ touse.moplaf.job/com.misc.touse.moplaf.job.emf.editor/src/com/misc/touse/moplaf/tousejob/presentation/ToUseJobActionBarContributor.java
@@ -29,7 +29,7 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.action.SubContributionItem;
-
+import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -39,8 +39,11 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
 
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.PreferencesUtil;
 
 import com.misc.common.moplaf.emf.editor.Util;
 import com.misc.common.moplaf.emf.editor.action.CancelAction;
@@ -99,6 +102,22 @@ public class ToUseJobActionBarContributor
 	 * @generated
 	 */
 	protected ISelectionProvider selectionProvider;
+
+	/**
+	 * This action opens the Preferences dialog.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 */
+	protected IAction showPreferencesDialogAction =
+			new Action("Preferences") {
+			@Override
+			public void run() {
+				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				PreferenceDialog dialog = PreferencesUtil.createPreferenceDialogOn(shell, null, null, null);
+				dialog.open();
+			}
+		};
+
 
 	/**
 	 * This action opens the Properties view.
@@ -228,6 +247,12 @@ public class ToUseJobActionBarContributor
 	@Override
 	public void contributeToMenu(IMenuManager menuManager) {
 		super.contributeToMenu(menuManager);
+
+		// Preferences menu
+		IContributionItem windowMenuManager = menuManager.find("window")  ;
+		IMenuManager windowMenuManager2 = (IMenuManager)windowMenuManager;
+		windowMenuManager2.add(new Separator("prefs"));
+		windowMenuManager2.add(this.showPreferencesDialogAction);
 
 		IMenuManager submenuManager = new MenuManager(TousejobEditorPlugin.INSTANCE.getString("_UI_ToUseJobEditor_menu"), "com.misc.touse.moplaf.tousejobMenuID");
 		menuManager.insertAfter("additions", submenuManager);
