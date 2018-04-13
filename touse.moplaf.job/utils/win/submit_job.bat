d02783e9c1e15f296a2ba5ddfedaac63ceda714e
MiSc <MiSc@ORD02652.ordina.belgium>
2018-04-13 17:48:19 +0200

    call examples

--- touse.moplaf.job/utils/win/submit_job.bat
+++ touse.moplaf.job/utils/win/submit_job.bat
@@ -1,6 +1,2 @@
-rem "C:\Program Files (x86)\GnuWin32\bin\wget.exe" "http://localhost:8081/default_path/submit"  --post-file=file="C:\git\touse.jobsched\touse.moplaf.job\utils\win\job.xml"
-rem "C:\Program Files (x86)\GnuWin32\bin\wget.exe" "http://localhost:8081/default_path/submit"  
-rem "C:\Program Files (x86)\GnuWin32\bin\wget.exe"  --header="Content-type: multipart/form-data boundary=FILEUPLOAD" --post-file="job.xml" "http://localhost:8081/default_path/submit?scheme=michelscheme"
-
 "C:\Program Files (x86)\GnuWin32\bin\wget.exe"  "http://localhost:8081/default_path/submitjob?filename=job.xml"
 
