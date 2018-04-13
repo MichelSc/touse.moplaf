4cb1635793c670f4ca1b17a1086d030e5255b2d7
MiSc <MiSc@ORD02652.ordina.belgium>
2018-04-13 14:16:51 +0200

    Implement post and submit file

--- touse.moplaf.job/utils/win/post_job.bat
+++ touse.moplaf.job/utils/win/post_job.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\GnuWin32\bin\wget.exe"  --post-file="job.xml" "http://localhost:8081/default_path/submitjob?jobtype=abc"
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe"  --post-file="job.xml" "http://localhost:8081/default_path/submitjob"
---
+++ touse.moplaf.job/utils/win/submit_file.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe"  "http://localhost:8081/default_path/submitfile?filename=import.xlsx&jobtype=be.ordina.sco.demos.shiftcoverop.emf.run_factory.mainjob"
+
