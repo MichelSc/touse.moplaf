86e33a6891eed71597fbe8caf1cc322c347cded1
MiSc <MiSc@ORD02652.ordina.belgium>
2018-04-16 14:36:08 +0200

    Do not let wget pickup a name for the downloaded file

---
+++ touse.moplaf.job/utils/win/get_file_result.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\GnuWin32\bin\wget.exe"  --content-disposition  "http://localhost:8081/default_path/getfileresult?submitid=2"
+rem "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --content-disposition  "http://127.0.0.1:63798/path_jobscheduler?servicehandler=download&cid=959a43b5&data=c:\tmp\coveropout.xlsx"
