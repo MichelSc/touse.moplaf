2a8c3df2c71718c418be72cdd6f91d3cbf76c11a
MiSc <MiSc@ORD02652.ordina.belgium>
2018-05-23 18:33:55 +0200

    Rework and debug scheduler

--- touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf/src/com/misc/touse/moplaf/tousescheduler/impl/ToUseScheduleLoadUnloadImpl.java
+++ touse.moplaf.scheduler/com.misc.touse.moplaf.scheduler.emf/src/com/misc/touse/moplaf/tousescheduler/impl/ToUseScheduleLoadUnloadImpl.java
@@ -2,6 +2,7 @@
  */
 package com.misc.touse.moplaf.tousescheduler.impl;
 
+import com.misc.common.moplaf.common.EnabledFeedback;
 import com.misc.common.moplaf.scheduler.Resource;
 import com.misc.common.moplaf.scheduler.ScheduleAfter;
 import com.misc.common.moplaf.scheduler.SchedulerFactory;
@@ -48,6 +49,22 @@ public class ToUseScheduleLoadUnloadImpl extends ToUseActionLoadUnloadImpl imple
 				                           shipment==null ? "null" : shipment.getName() );
 		return description;
 	}
+	
+	@Override
+	public EnabledFeedback getValidFeedback() {
+		EnabledFeedback super_enabled = super.getValidFeedback();
+		if ( !super_enabled.isEnabled() ) {
+			return super_enabled;
+		}
+		
+		Shipment shipment = this.getShipment();
+		if ( shipment==null) {
+			return new EnabledFeedback(false, "No Shipment selected");
+		}
+		
+		return EnabledFeedback.NOFEEDBACK;
+	}
+	
 
 	@Override
 	protected void initializeImpl() {
@@ -58,39 +75,55 @@ public class ToUseScheduleLoadUnloadImpl extends ToUseActionLoadUnloadImpl imple
 		ToUseLoadShipment loadTask = schedule.getTaskLoad(shipment);
 		ToUseUnloadShipment unloadTask = loadTask.getUnloadShipment();
 		
-		for (Resource resource : schedule.getResources() ) {
-			Task loadInsertionPoint = null;
-			do {
-				// schedule load
-				ScheduleAfter scheduleLoad = SchedulerFactory.eINSTANCE.createScheduleAfter();
-				scheduleLoad.setInsertionPoint(loadInsertionPoint);
-				scheduleLoad.setResource(resource);
-				scheduleLoad.setTaskToSchedule(loadTask);
-				scheduleLoad.setSolution(false);
-				this.getStartDeltas().add(scheduleLoad);
-				
-				// loop control over the load insertions points
-				loadInsertionPoint = loadInsertionPoint==null
-						           ? resource.getFirstTask()
-							       : loadInsertionPoint.getNextTask();
+		if ( loadTask.isScheduled() ) {
+			if ( unloadTask.isScheduled() ) {
+				// nothing to do
+			} else {
+				// schedule the unload on the same resource as the load, and after this latter
+				// TODO
+			}
+		} else {
+			if ( unloadTask.isScheduled() ) {
+				// schedule the load on the same resource as the unload, but before
+				// TODO
+			} else {
+				// both are not scheduled, schedule both on some resource
+				for (Resource resource : schedule.getResources() ) {
+					Task loadInsertionPoint = null;
+					do {
+						// schedule load
+						ScheduleAfter scheduleLoad = SchedulerFactory.eINSTANCE.createScheduleAfter();
+						scheduleLoad.setInsertionPoint(loadInsertionPoint);
+						scheduleLoad.setResource(resource);
+						scheduleLoad.setTaskToSchedule(loadTask);
+						scheduleLoad.setSolution(false);
+						this.getStartDeltas().add(scheduleLoad);
+						
+						// loop control over the load insertions points
+						loadInsertionPoint = loadInsertionPoint==null
+								           ? resource.getFirstTask()
+									       : loadInsertionPoint.getNextTask();
 
-				// schedule unload
-				Task unloadInsertionPoint = loadTask;
-				do {
-					ScheduleAfter scheduleUnload = SchedulerFactory.eINSTANCE.createScheduleAfter();
-					scheduleUnload.setInsertionPoint(unloadInsertionPoint);
-					scheduleUnload.setResource(resource);
-					scheduleUnload.setTaskToSchedule(unloadTask);
-					scheduleUnload.setSolution(true);
-					scheduleLoad.getNextDeltas().add(scheduleUnload);
-					// loop control over the unload insertion points
-					unloadInsertionPoint = unloadInsertionPoint == loadTask
-							             ? loadInsertionPoint
-							             : unloadInsertionPoint.getNextTask();
-				} while ( unloadInsertionPoint !=null );
-				
-			} while (loadInsertionPoint !=null);
-		}  // traverse the Resources
+						// schedule unload
+						Task unloadInsertionPoint = loadTask;
+						do {
+							ScheduleAfter scheduleUnload = SchedulerFactory.eINSTANCE.createScheduleAfter();
+							scheduleUnload.setInsertionPoint(unloadInsertionPoint);
+							scheduleUnload.setResource(resource);
+							scheduleUnload.setTaskToSchedule(unloadTask);
+							scheduleUnload.setSolution(true);
+							scheduleLoad.getNextDeltas().add(scheduleUnload);
+							// loop control over the unload insertion points
+							unloadInsertionPoint = unloadInsertionPoint == loadTask
+									             ? loadInsertionPoint
+									             : unloadInsertionPoint.getNextTask();
+						} while ( unloadInsertionPoint !=null );
+						
+					} while (loadInsertionPoint !=null);
+				}  // traverse the Resources
+			}
+		}
+		
 	} // crerateDeltasImpl
 
 } //ToUseScheduleLoadUnloadImpl
